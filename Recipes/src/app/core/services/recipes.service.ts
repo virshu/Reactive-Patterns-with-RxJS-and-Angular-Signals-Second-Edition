e2867dacf396e3a477077a0f922a05c6849a960b
virshu <felix@rabinovich.org>
2024-12-28 23:26:01 -0800

    Ch3 - Fetching Data as streams

--- Recipes/src/app/core/services/recipes.service.ts
+++ Recipes/src/app/core/services/recipes.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  recipes$ = this.http.get<Recipe[]>(`${environment.basePath}/recipes`);
-  constructor(private http: HttpClient) { }
+  recipes$: Observable<Recipe[]>;
+  constructor(private http: HttpClient) { 
+    this.recipes$ = this.http.get<Recipe[]>(`${environment.basePath}/recipes`);
+  }
 }
--- Recipes/src/app/recipes-list/recipes-list.component.ts
+++ Recipes/src/app/recipes-list/recipes-list.component.ts
@@ -4,10 +4,14 @@ import { DataViewModule } from 'primeng/dataview';
 import { RatingModule } from 'primeng/rating';
 import { RecipesService } from '../core/services/recipes.service';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Recipe } from '../core/model/recipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipes-list',
   imports: [
+    FormsModule,
     DataViewModule,
     ButtonModule,
     RatingModule,
@@ -17,8 +21,10 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './recipes-list.component.scss'
 })
 export class RecipesListComponent {
-  recipes$ = this.service.recipes$;
-
-  constructor(private service: RecipesService) { }
+    recipes$: Observable<Recipe[]>;
+  
+    constructor(private service: RecipesService) { 
+      this.recipes$ = this.service.recipes$;
+    }
 
 }
