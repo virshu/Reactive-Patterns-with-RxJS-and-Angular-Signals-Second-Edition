2cd9ce818d85d3facca6dfd2bcfe1d980cbd17f6
virshu <felix@rabinovich.org>
2024-12-29 11:29:37 -0800

    Created RecipesFilter component

--- Recipes/src/app/home/home.component.ts
+++ Recipes/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RecipesListComponent } from '../recipes-list/recipes-list.component';
+import { RecipesFilterComponent } from "../recipes-filter/recipes-filter.component";
 
 @Component({
   selector: 'app-home',
   imports: [
-    RecipesListComponent
-  ],
+    RecipesListComponent,
+    RecipesFilterComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ Recipes/src/app/recipes-filter/recipes-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-filter',
+  imports: [],
+  templateUrl: './recipes-filter.component.html',
+  styleUrl: './recipes-filter.component.scss'
+})
+export class RecipesFilterComponent {
+
+}
