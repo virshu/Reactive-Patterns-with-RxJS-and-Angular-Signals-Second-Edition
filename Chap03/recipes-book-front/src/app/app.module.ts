bbae4ebf36721446e88a2396db576e358e43d1c7
virshu <felix@rabinovich.org>
2024-12-28 16:38:56 -0800

    made primeng work
    Any manual that you follow is either obsolete, or confusing, or simply wrong!

--- Chap03/recipes-book-front/src/app/app.module.ts
+++ Chap03/recipes-book-front/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { RecipesService } from './core/services/recipes.service';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './core/header/header.component';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 @NgModule({ declarations: [
         AppComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
         AppRoutingModule,
-        HeaderComponent], providers: [RecipesService, provideHttpClient(withInterceptorsFromDi())] })
+        HeaderComponent], providers: [
+            RecipesService, 
+            provideHttpClient(withInterceptorsFromDi()),
+            providePrimeNG({
+                theme: {
+                    preset: Aura
+                }
+            })
+        ] })
 export class AppModule { }
