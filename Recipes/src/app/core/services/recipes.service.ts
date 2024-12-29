4d76a294f44367d62fe13bd7a0583e5f543ba60e
virshu <felix@rabinovich.org>
2024-12-28 23:41:25 -0800

    Ch4: error handling

--- Recipes/src/app/core/services/recipes.service.ts
+++ Recipes/src/app/core/services/recipes.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,7 @@ export class RecipesService {
 
   recipes$: Observable<Recipe[]>;
   constructor(private http: HttpClient) { 
-    this.recipes$ = this.http.get<Recipe[]>(`${environment.basePath}/recipes`);
+    this.recipes$ = this.http.get<Recipe[]>(`${environment.basePath}/recipes`)
+      .pipe(catchError(() => of([])));
   }
 }
